36448036bf37bb4dc2d26621cdda82f83cacde51
sandeepdevopscloud <127117888+sandeepdevopscloud@users.noreply.github.com>
2024-01-09 08:39:49 +0530

    Update key

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ access_key = "AKIAYTTNYJL2GAJ7R4FH"
 secret_key = "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"
 }
 
-resource "aws_instance" "key" {
+resource "aws_instance" "one" {
 ami = "ami-00b8917ae86a424c9"
 instance_type = "t2.micro"
 key_name = "mykey"
