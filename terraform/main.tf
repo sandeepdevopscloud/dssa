89df4f4fb463987d244f8e140bd0adef73fb7037
sandeepdevopscloud <127117888+sandeepdevopscloud@users.noreply.github.com>
2024-01-09 08:30:55 +0530

    Update

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,13 @@
 provider "aws" {
 region = "us-east-1"
-access_key = "AKIAYTTNYJL2EBWSS6E3"
-secret_key = "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"
+access_key = "AKIAYTTNYJL2GAJ7R4FH"
+secret_key = "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"
 }
 
 resource "aws_instance" "key" {
 ami = "ami-00b8917ae86a424c9"
 instance_type = "t2.micro"
-key_name = "Kernal-keypair"
+key_name = "mykey"
 vpc_security_group_ids = [aws_security_group.demo-sg.id]
 tags = {
 Name = "terraform"
